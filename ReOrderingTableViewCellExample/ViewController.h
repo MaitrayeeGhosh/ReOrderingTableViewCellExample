f7d4b4b7bbc3322757e0ed9abd148af82b4745df
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-02-17 21:02:10 +0530

    Initial Commit

---
+++ ReOrderingTableViewCellExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReOrderingTableViewCellExample
+//
+//  Created by Maitrayee Ghosh on 17/02/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReOrderingTableViewCellExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReOrderingTableViewCellExample
+//
+//  Created by Maitrayee Ghosh on 17/02/16.
+//  Copyright © 2016 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
