15c42652996422c041a898ed457090d19dbcf8d6
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2016-02-17 22:08:59 +0530

    This is a simple example of UITableView Cell Re-Ordering in iOS app.

--- ReOrderingTableViewCellExample/ViewController.h
+++ ReOrderingTableViewCellExample/ViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+{
+    NSMutableArray *dataArray;
+}
+@property (weak, nonatomic) IBOutlet UITableView *mTableView;
 
 
 @end
--- ReOrderingTableViewCellExample/ViewController.m
+++ ReOrderingTableViewCellExample/ViewController.m
@@ -16,9 +16,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    dataArray=[[NSMutableArray alloc] initWithObjects:
+                                    @"Apple",
+                                    @"Mango",
+                                    @"Papaya",
+                                    @"Guava",
+                                    @"Pineapple",
+                                    @"Strawberry",
+                                    @"Banana",
+                                    @"Grapes",
+                                    @"Pomegranate",
+                                    @"Green Tea",
+                                    @"Raisin",
+                                    nil];
+    
+    self.mTableView.delegate=(id)self;
+    self.mTableView.dataSource=(id)self;
+   [self.mTableView setEditing:YES animated:YES];
 }
 
+#pragma mark - UITableView Delegate and DataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [dataArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleNone;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [dataArray exchangeObjectAtIndex:sourceIndexPath.row withObjectAtIndex:destinationIndexPath.row];
+    [self.mTableView reloadData];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
